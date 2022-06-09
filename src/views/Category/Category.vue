5d4f8172aaee2b5ae31206d1d1ad8fc6f32684f1
ahmedopeyemikabiru <opeyemi.kabiru@yahoo.com>
2022-06-09 23:22:26 +0100

    Implement view categories UI

---
+++ src/views/Category/Category.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h3 class="pt-3">Our Categories</h3>
+        <router-link id="add-category" :to="{name : 'AddCategory'}" v-show="$route.name==='AdminCategory'">
+          <button class="btn">Add a new Category</button>
+        </router-link>
+      </div>
+    </div>
+    <div class="row">
+      <div v-for="category of categories" :key="category.id" class="col-md-6 col-xl-4 col-12 pt-3  justify-content-around d-flex">
+        <CategoryBox :category="category">
+        </CategoryBox>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CategoryBox from "@/components/Category/CategoryBox";
+const axios = require("axios")
+
+export default {
+  name: "ViewCategory",
+  components: {CategoryBox},
+  data(){
+    return {
+      baseURL : "http://localhost:8080/",
+      categories : null,
+    }
+  },
+  methods: {
+    async getCategories() {
+      //fetch categories
+      await axios.get(this.baseURL + "category")
+          .then(res => this.categories = res.data)
+          .catch(err => console.log(err))
+    }
+  },
+  mounted() {
+    this.getCategories()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
